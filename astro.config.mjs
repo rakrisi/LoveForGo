197f7c77fb4dc7d519ac506b565a819eb87bcc1c
rakrisi <radhesingh12321@gmail.com>
2024-11-09 02:40:28 +0530

    chore: Update base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'http://rakrisi.com/loveforgo/',
+	base: '/loveforgo/loveforgo/dist',
 	integrations: [
 		starlight({
 			title: 'LoveForGo',
